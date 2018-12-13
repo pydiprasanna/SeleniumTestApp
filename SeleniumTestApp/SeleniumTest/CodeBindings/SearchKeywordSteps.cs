b38d7ac32d6ac9cdc79300501a2c13430532e9c4
pydiprasanna <prasannakumari.pydi8@gmail.com>
2018-12-13 19:58:13 +0530

    initial commit

---
+++ SeleniumTestApp/SeleniumTest/CodeBindings/SearchKeywordSteps.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+using System.Threading;
+
+namespace SeleniumTest.CodeBindings
+{
+    [Binding]
+    public class SearchKeywordSteps
+    {
+        private IWebDriver _driver;
+
+        [Given(@"I am on google search page")]
+        public void GivenIAmOnGoogleSearchPage()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("http://www.google.com/ncr");
+        }
+        
+        [When(@"I Search for a keyword called '(.*)'")]
+        public void WhenISearchForAKeywordCalled(string keyword)
+        {
+            ScenarioContext.Current.Pending(); IWebElement query = _driver.FindElement(By.Name("q"));
+            query.SendKeys(keyword);
+            query.Submit();
+        }
+        
+        [Then(@"I should see '(.*)' on the goole search")]
+        public void ThenIShouldSeeOnTheGooleSearch(string keyword)
+        {
+            Thread.Sleep(5000);
+            Assert.That(_driver.Title, Is.EqualTo(keyword));
+        }
+    }
+}
---
+++ SeleniumTestApp/SeleniumTest/Features/SearchKeyword.feature
@@ -0,0 +1,6 @@
+﻿Feature: SearchKeyword
+
+Scenario: Verify Search Keyword in google
+	Given I am on google search page
+	When I Search for a keyword called 'Selenium'
+	Then I should see 'Selenium' on the goole search
